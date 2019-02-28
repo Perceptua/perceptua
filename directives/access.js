070d166153caaf38dbc413682916459b16dd72ed
Avery Herkel <aherkel09@gmail.com>
2019-02-27 22:37:07 -0500

    Update and rename info.js to access.js

---
+++ directives/access.js
@@ -0,0 +1,18 @@
+app.directive('info', function() { 
+  return { 
+    restrict: 'E', 
+    scope: {}, 
+    templateUrl: 'directives/access.html',
+    link: function(scope, element, attrs) {
+      scope.accessText = "Check Access Code",
+      scope.access = false,
+        
+      scope.access = function() {
+        if(!scope.access) {
+          scope.accessText = "Welcome";
+          scope.access = true;
+        }
+      }
+    } 
+  }; 
+});
