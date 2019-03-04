6163dadc393ca08bd3980a480ad8f3b90c2da5f2
Avery Herkel <aherkel09@gmail.com>
2019-03-04 14:09:21 -0500

    remove nav click

--- directives/about.js
+++ directives/about.js
@@ -4,12 +4,11 @@ app.directive('about', function() {
     scope: {}, 
     templateUrl: 'directives/about.html',
     link: function(scope, element, attrs) {
-      $('#about').click(function() {
-        $('about').first().fadeIn('slow');
-      });
+
       scope.hideAbout = function() {
         $('about').first().fadeOut('slow');
       }
+      
     }
   };
 });
