26e21003b01410353b1f811cf4427a7fd52da366
Avery Herkel <aherkel09@gmail.com>
2019-03-04 13:56:02 -0500

    Update and rename upcoming.js to content.js

---
+++ directives/content.js
@@ -0,0 +1,14 @@
+app.directive('content', function() { 
+  return { 
+    restrict: 'E', 
+    scope: {
+      creator: '=',
+    }, 
+    templateUrl: 'directives/creator.html',
+    link: function(scope, element, attrs) {
+      scope.hideContent = function(ev) {
+        $(ev.target).nearest('.content-container').fadeOut('slow');
+      }
+    }
+  };
+});
