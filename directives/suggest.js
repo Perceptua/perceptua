3605f40ae053ffd03f09795822d5a365c11411bd
Avery Herkel <aherkel09@gmail.com>
2019-03-04 11:59:12 -0500

    indicate submission

--- directives/suggest.js
+++ directives/suggest.js
@@ -4,12 +4,22 @@ app.directive('suggest', function() {
     scope: {}, 
     templateUrl: 'directives/suggest.html',
     link: function(scope, element, attrs) {
+      
       $('#suggest').click(function() {
         $('suggest').first().fadeIn('slow');
       });
+      
       scope.hideSuggest = function() {
         $('suggest').first().fadeOut('slow');
       }
+      
+      scope.suggestCreator = function() {
+        $('#received-suggestion').fadeIn('slow');
+        setTimeout(function() {
+          $('suggest').first().fadeOut('slow');
+        }, 2000);
+      }
+      
     }
   };
 });
