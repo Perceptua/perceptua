99dda76edab300b2e009574d02d98cfe0f170dc5
Avery Herkel <aherkel09@gmail.com>
2019-03-04 14:15:56 -0500

    remove function

--- directives/creator.js
+++ directives/creator.js
@@ -6,9 +6,6 @@ app.directive('creator', function() {
     }, 
     templateUrl: 'directives/creator.html',
     link: function(scope, element, attrs) {
-      scope.hideContent = function(ev) {
-        $(ev.target).nearest('.content-container').fadeOut('slow');
-      }
     }
   };
 });
