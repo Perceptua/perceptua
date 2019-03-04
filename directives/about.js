8003203659513d614920f5a859478d67d669c1c9
Avery Herkel <aherkel09@gmail.com>
2019-03-04 08:27:01 -0500

    Create about.js

---
+++ directives/about.js
@@ -0,0 +1,10 @@
+app.directive('about', function() { 
+  return { 
+    restrict: 'E', 
+    scope: {}, 
+    templateUrl: 'directives/about.html',
+    link: function(scope, element, attrs) {
+      
+    }
+  };
+});
