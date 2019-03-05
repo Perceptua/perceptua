31d73f7a2bdacd55fa458cf430e25ddb89696349
Avery Herkel <aherkel09@gmail.com>
2019-03-04 23:09:30 -0500

    show/hide footer

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -19,19 +19,26 @@ app.controller('MainController', ['$scope', function($scope) {
 }]);
 
 function hideContent(ev) {
-  $(ev.target).closest('.content-container')
-    .fadeOut('slow')
-    .removeClass('viewing');
+  $('footer').first().fadeOut('slow');
+  var content = $(ev.target).closest('.content-container');
+  content.fadeOut('slow', function() {
+    content.removeClass('viewing');
+    $('footer').first().fadeIn('slow');
+  });
 }
 
 function showContent(content) {
+  $('footer').first().fadeOut('slow');
   var viewing = $('.viewing').first();
   if (viewing.length > 0) {
     viewing.fadeOut('slow', function() {
       viewing.removeClass('viewing');
       $(content + '-container').fadeIn('slow').addClass('viewing');
+      $('footer').first().fadeIn('slow');
     });
   } else {
-    $(content + '-container').fadeIn('slow').addClass('viewing');
+    $(content + '-container').fadeIn('slow', function() {
+      $('footer').first().fadeIn('slow');
+    }).addClass('viewing');
   }
 }
