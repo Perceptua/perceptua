eb08bbc4c96bbeb4aea60be7db15acd81fad0738
Avery Herkel <aherkel09@gmail.com>
2019-03-04 23:45:40 -0500

    remove function

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -18,11 +18,6 @@ app.controller('MainController', ['$scope', function($scope) {
   ];
 }]);
 
-$(document).ready(function() {
-  $('#featured-container').fadeIn('slow');
-  $('footer').first().fadeIn('slow');
-});
-
 function hideContent(ev) {
   $('footer').first().fadeOut('slow');
   var content = $(ev.target).closest('.content-container');
