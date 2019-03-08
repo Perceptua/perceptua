f8e96256ab8ba46eb184686fca55414f587c146e
Avery Herkel <aherkel09@gmail.com>
2019-03-08 13:06:06 -0500

    Update access.js

--- directives/access.js
+++ directives/access.js
@@ -5,37 +5,43 @@ app.directive('access', function() {
     templateUrl: 'directives/access.html',
     link: function(scope, element, attrs) {
       
-      scope.hasAccess = false;
-      scope.accessCode = null;
-      firebase.firestore().collection('featured').doc('access').get().then(function(doc) {
-        if (doc.exists) {
-          scope.accessCode = doc.data().code;
-        }
-      });
-      
+      scope.createUser = function(email, code) {
+        firebase.auth().createUserWithEmailAndPassword(email, code);
+      }
+     
       scope.grantAccess = function() {
         $('footer').first().fadeOut('slow');
-        $('#featured-container').fadeOut('slow', function() {
+        $('#access-container').removeClass('viewing').fadeOut('slow', function() {
+          $('#access-input').css('border', '0.25em solid #var(--dark)');
           $('#welcome').fadeIn('slow');
           setTimeout(function() {
             $('#welcome').fadeOut('slow', function() {
+              $('#featured-container').addClass('viewing').fadeIn('slow');
               $('footer').first().fadeIn('slow');
             });
           }, 1500);
         });
       }
+      
+      scope.failAccess = function() {
+        $('#access-input').css('border', '0.25em solid #990033');
+        $('#error').append('<p style="font-size:0.75em;">Incorrect Access Code. Please Try Again.</p>');
+      }
               
       scope.checkAccess = function() {
         $('#error').empty();
+        var email = $('#email-input').val();
         var code = $('#access-input').val();
-        if (!scope.hasAccess && scope.accessCode && code == scope.accessCode) {
-          scope.hasAccess = true;
-          $('#access-input').css('border', '0.25em solid #var(--dark)');
-          scope.grantAccess();
-        } else {
-          $('#access-input').css('border', '0.25em solid #990033');
-          $('#error').append('<p style="font-size:0.75em;">Incorrect Access Code. Please Try Again.</p>');
-        }
+        
+        firebase.auth().signInWithEmailAndPassword(email, code).catch(function(error) {
+          if (error.code == 'auth/user-not-found') {
+            scope.createUser(email, code);
+          } else if (error) {
+            scope.failAccess();
+          } else {
+            scope.grantAccess();
+          }
+        });
       }
       
       scope.showAccessHelp = function() {
