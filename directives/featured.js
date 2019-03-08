3704c809f1c231b9126dda1939b2815cb845654a
Avery Herkel <aherkel09@gmail.com>
2019-03-08 12:33:47 -0500

    change template name

--- directives/featured.js
+++ directives/featured.js
@@ -2,7 +2,7 @@ app.directive('featured', function() {
   return { 
     restrict: 'E', 
     scope: {}, 
-    templateUrl: 'directives/creator.html',
+    templateUrl: 'directives/featured.html',
     link: function(scope, element, attrs) {
       /*
       firebase.storage().ref('public/Destruction_of_Leviathan.png').getDownloadURL().then(function(url) {
