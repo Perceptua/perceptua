51a7e289824b8e33ab055551cca6590da9efc055
Avery Herkel <aherkel09@gmail.com>
2019-03-15 19:07:28 -0400

    Update MainController.js

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,25 +1,25 @@
 app.controller('MainController', ['$scope', function($scope) { 
   $scope.title = 'perceptua';
   $scope.featured = {
-    id: 3,
+    id: 2,
     name: 'Ludwig van Beethoven',
     medium: 'Music',
   };
   $scope.upcoming = [
     {
-      id: 4,
+      id: 3,
       name: 'Johannes Brahms',
       medium: 'Music',
     }
   ];
   $scope.all = [
     {
-      id: 1,
+      id: 0,
       name: 'Maxfield Parrish',
       medium: 'Art',
     },
     {
-      id: 2,
+      id: 1,
       name: 'Stanley Kubrick',
       medium: 'Film',
     }
