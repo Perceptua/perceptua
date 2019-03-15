4bd8caf646384f18772872a2a9e81275ca9745a4
Avery Herkel <aherkel09@gmail.com>
2019-03-15 13:16:09 -0400

    remove functions

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -24,36 +24,5 @@ app.controller('MainController', ['$scope', function($scope) {
       medium: 'Film',
     }
   ];
-  
 }]);
-
-function hideContent(ev) {
-  $('footer').first().fadeOut('slow');
-  var content = $(ev.target).closest('.content-container');
-  content.fadeOut('slow', function() {
-    content.removeClass('viewing');
-    $('footer').first().fadeIn('slow');
-  });
-}
-
-function showContent(content) {
-  var viewing = $('.viewing').first();
-  if (viewing.length > 0) {
-    viewing.fadeOut('slow', function() {
-      viewing.removeClass('viewing');
-      $(content + '-container').fadeIn('slow').addClass('viewing');
-      $('footer').first().fadeIn('slow');
-    });
-  } else {
-    $(content + '-container').fadeIn('slow', function() {
-      $('footer').first().fadeIn('slow');
-    }).addClass('viewing');
-  }
-}
-
-function redirectTo(content) {
-  $('#content').first().fadeOut('slow', function() {
-    window.location.replace('https://perceptua.blue/#/' + content);
-  });
-}
   
