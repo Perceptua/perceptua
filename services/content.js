eef7d19c0cf0611dfb70d6ee05ac8ef08e85e0be
Avery Herkel <aherkel09@gmail.com>
2019-03-15 10:21:34 -0400

    get download url

--- services/content.js
+++ services/content.js
@@ -1,5 +1,9 @@
 app.factory('content', ['$http', function($http) {
-  return $http.get('url')
+  
+  // Use Firebase Storage (included in index.html) to get content data url
+  var url = firebase.storage().ref('public/content.json').getDownloadURL();
+  
+  return $http.get(url)
          .success(function(data) {
            return data;
          })
