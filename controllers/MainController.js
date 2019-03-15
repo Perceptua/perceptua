27b7bd96a89fed277d41d4a8022dd15e2043bbb3
Avery Herkel <aherkel09@gmail.com>
2019-03-15 12:19:12 -0400

    add ids

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,21 +1,25 @@
 app.controller('MainController', ['$scope', function($scope) { 
   $scope.title = 'perceptua';
   $scope.featured = {
+    id: 3,
     name: 'Ludwig van Beethoven',
     medium: 'Music',
   };
   $scope.upcoming = [
     {
+      id: 4,
       name: 'Johannes Brahms',
       medium: 'Music',
     }
   ];
   $scope.all = [
     {
+      id: 1,
       name: 'Maxfield Parrish',
       medium: 'Art',
     },
     {
+      id: 2,
       name: 'Stanley Kubrick',
       medium: 'Film',
     }
