877c11efd1b11ae5b0670a13d3b813aff0bff52a
Avery Herkel <aherkel09@gmail.com>
2019-03-15 11:57:14 -0400

    return data

--- services/content.js
+++ services/content.js
@@ -1,22 +1,13 @@
 app.factory('content', ['$http', function($http) {
   
   // Use Firebase Storage (included in index.html) to get content data url
-  var contentData = null;
   firebase.storage().ref('public/content.json').getDownloadURL().then(function(url) {
-    contentData = url;
-  });
-  
-  console.log(contentData);
-  
-  if (contentData) {
-    return $http.get(contentData)
+    return $http.get(url)
       .success(function(data) {
         return data;
       })
       .error(function(data) {
         return data;
       });
-  }
-  
-  return false;
+  });
 }]);
