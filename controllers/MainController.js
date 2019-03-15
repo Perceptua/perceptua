4d4d22eafb4424744629cb8f470d787031438afb
Avery Herkel <aherkel09@gmail.com>
2019-03-15 13:06:43 -0400

    add redirect function

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -50,3 +50,10 @@ function showContent(content) {
     }).addClass('viewing');
   }
 }
+
+function redirectTo(content) {
+  $('footer').first().fadeOut('slow', function() {
+    window.location.replace('https://perceptua.blue/#/' + content);
+  });
+}
+  
