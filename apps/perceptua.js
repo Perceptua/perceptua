5c31fb2d905f26e6665a803b72fe1a87ff5e3c66
Avery Herkel <aherkel09@gmail.com>
2019-03-15 12:45:17 -0400

    change url routes

--- apps/perceptua.js
+++ apps/perceptua.js
@@ -2,15 +2,31 @@ var app = angular.module('perceptua', ['ngRoute']);
 
 app.config(function($routeProvider) {
   $routeProvider
-  .when("/", {
+  .when("/featured", {
     controller: 'MainController',
-    templateUrl: "views/home.html"
+    templateUrl: "views/featured.html"
+  })
+  .when("/upcoming", {
+    controller: 'MainController',
+    templateUrl: "views/upcoming.html"
+  })
+  .when("/all", {
+    controller: 'MainController',
+    templateUrl: "views/all.html"
+  })
+  .when("/suggest", {
+    controller: 'MainController',
+    templateUrl: "views/suggest.html"
+  })
+  .when("/about", {
+    controller: 'MainController',
+    templateUrl: "views/about.html"
   })
   .when('/content/:id', {
     controller: 'ContentController',
     templateUrl: 'views/content.html',
   })
   .otherwise({
-    redirectTo: "/"
+    redirectTo: "/featured"
   });
 });
