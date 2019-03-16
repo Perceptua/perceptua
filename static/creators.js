009a8a5e45557fc65704a5fdbd1998476625ed03
Avery Herkel <aherkel09@gmail.com>
2019-03-15 21:22:47 -0400

    Create creators.js

---
+++ static/creators.js
@@ -0,0 +1,28 @@
+export const featured = [
+  {
+    id: 2,
+    name: 'Ludwig van Beethoven',
+    medium: 'Music',
+  }
+];
+
+export const upcoming = [
+  {
+    id: 3,
+    name: 'Johannes Brahms',
+    medium: 'Music',
+  }
+];
+  
+export const all = [
+  {
+    id: 0,
+    name: 'Maxfield Parrish',
+    medium: 'Art',
+  },
+  {
+    id: 1,
+    name: 'Stanley Kubrick',
+    medium: 'Film',
+  }
+];
