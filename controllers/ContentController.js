ea31ab0375c1a34ad866308b384c06c9161e89e6
Avery Herkel <aherkel09@gmail.com>
2019-03-15 20:55:49 -0400

    Update ContentController.js

--- controllers/ContentController.js
+++ controllers/ContentController.js
@@ -2,6 +2,7 @@ app.controller('ContentController', ['$scope', '$routeParams', function($scope, 
   firebase.firestore().collection('creators').doc($routeParams.id).get().then(function(doc) {
     if (doc.exists) {
       $scope.creator = doc.data();
+      console.log($scope.creator);
     }
   }).catch(function(error) {
     console.log(error);
