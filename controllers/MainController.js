571e67a17d2457303c842c8b7259e3d6ae110cba
Avery Herkel <aherkel09@gmail.com>
2019-03-15 22:37:22 -0400

    Update MainController.js

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -20,8 +20,6 @@ app.controller('MainController', ['$scope', '$routeParams', function($scope, $ro
       id: 0,
       name: 'Maxfield Parrish',
       medium: 'Art',
-      content: 'https://firebasestorage.googleapis.com/v0/b/perceptua-b6ea3.appspot.com/o/public%2Ffeatured.html' + 
-               '?alt=media&token=0ff1e240-349d-4dd0-a188-8c47619be97a',
     },
     {
       id: 1,
