125017cc84d603d639bb1956b917128200bb051f
Avery Herkel <aherkel09@gmail.com>
2019-03-15 20:41:20 -0400

    Update perceptua.js

--- apps/perceptua.js
+++ apps/perceptua.js
@@ -23,7 +23,7 @@ app.config(function($routeProvider) {
     templateUrl: "views/about.html"
   })
   .when('/content/:id', {
-    controller: 'MainController',
+    controller: 'ContentController',
     templateUrl: 'views/content.html',
   })
   .otherwise({
