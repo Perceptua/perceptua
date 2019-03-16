ab198187c65f659443372ce1e730e075cfd22b47
Avery Herkel <aherkel09@gmail.com>
2019-03-15 21:31:52 -0400

    revert

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,13 +1,34 @@
-import {featured, upcoming, all} from '/static/creators.js'
-
 app.controller('MainController', ['$scope', '$routeParams', function($scope, $routeParams) { 
   $scope.title = 'perceptua';
-  $scope.featured = featured;
-  $scope.upcoming = upcoming;
-  $scope.all = all;
   
-  if ($routeParams) {
+  $scope.featured = {
+    id: 2,
+    name: 'Ludwig van Beethoven',
+    medium: 'Music',
+  };
+  
+  $scope.upcoming = [
+    {
+      id: 3,
+      name: 'Johannes Brahms',
+      medium: 'Music',
+    }
+  ];
+  
+  $scope.all = [
+    {
+      id: 0,
+      name: 'Maxfield Parrish',
+      medium: 'Art',
+    },
+    {
+      id: 1,
+      name: 'Stanley Kubrick',
+      medium: 'Film',
+    }
+  ];
+  
+  if ($routeParams.id) {
     $scope.creator = $scope.all[$routeParams.id];
   }
 }]);
-  
