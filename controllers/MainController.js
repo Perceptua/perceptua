42b3d81d1167110c1ff8df0e1d9eda16f93f6f77
Avery Herkel <aherkel09@gmail.com>
2019-03-17 18:13:15 -0400

    Update MainController.js

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,26 +1,25 @@
 app.controller('MainController', ['$scope', '$routeParams', function($scope, $routeParams) { 
   $scope.title = 'perceptua';
   
-  $scope.all = {
-    'maxfield_parrish': {
-      name: 'Maxfield Parrish',
-      medium: 'Art',
-    },
-    'stanley_kubrick': {
-      name: 'Stanley Kubrick',
-      medium: 'Film',
-    },
-  };
+  $scope.all = {};
+  
+  firebase.firestore().collection('creators').get().then(function(docs) {
+    docs.forEach(function(doc) {
+      $scope.all[doc.id] = doc.data();
+    });
+  });
   
   $scope.featured = {
     name: 'Aldous Huxley',
     medium: 'Literature',
+    'added': new Date(2019, 03, 17, 17, 39, 00, 00);
   };  
   
   $scope.upcoming = {
     'johannes_brahms': {
       name: 'Johannes Brahms',
       medium: 'Music',
+      'added': new Date(2019, 03, 17, 17, 39, 00, 00);
     }
   };
   
