3d3b9e9a0e585bd13fc2911546a55261bfe45338
Avery Herkel <aherkel09@gmail.com>
2019-03-17 19:35:41 -0400

    Create creators.js

---
+++ services/creators.js
@@ -0,0 +1,10 @@
+app.factory('creators', [function() {
+  return firebase.firestore().collection('creators').get().then(function(docs) {
+    var creators = {};
+    docs.forEach(function(doc) {
+      creators[doc.id] = doc.data();
+    });
+  }).then(function() {
+    return creators;
+  });
+}]);
