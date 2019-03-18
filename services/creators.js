cdbc7cff4b202fbd962cfddbaeaa50d5daef8375
Avery Herkel <aherkel09@gmail.com>
2019-03-17 21:51:45 -0400

    return docs

--- services/creators.js
+++ services/creators.js
@@ -1,9 +1,5 @@
 app.factory('creators', [function() {
   return firebase.firestore().collection('creators').get().then(function(docs) {
-    var creators = {};
-    docs.forEach(function(doc) {
-      creators[doc.id] = doc.data();
-    });
-    return creators;
+    return docs;
   });
 }]);
