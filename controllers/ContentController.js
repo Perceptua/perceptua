8e9538689451cc9583762069668b311824fac0d3
Avery Herkel <aherkel09@gmail.com>
2019-03-18 09:09:11 -0400

    log data

--- controllers/ContentController.js
+++ controllers/ContentController.js
@@ -1,6 +1,7 @@
 app.controller('ContentController', ['$scope', 'creators', '$routeParams', function($scope, creators, $routeParams) {
   creators.then(function(data) {
     if ($routeParams.name) {
+      console.log(data[$routeParams.name]);
       $scope.creator = data[$routeParams.name];
     }
   });
