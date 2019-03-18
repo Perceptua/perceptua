c4f847e7f2ee1755da9b818f0c5dbd331e493d50
Avery Herkel <aherkel09@gmail.com>
2019-03-18 10:43:47 -0400

    Update ContentController.js

--- controllers/ContentController.js
+++ controllers/ContentController.js
@@ -1,37 +1,10 @@
 app.controller('ContentController', ['$scope', 'creators', '$routeParams', function($scope, creators, $routeParams) {
-  $scope.all = {
-    'maxfield_parrish': {
-      name: 'Maxfield Parrish',
-      medium: 'Art',
-    },
-    'stanley_kubrick': {
-      name: 'Stanley Kubrick',
-      medium: 'Film',
-    },
-  };
-  
-  $scope.featured = {
-    name: 'Aldous Huxley',
-    medium: 'Literature',
-  };  
-    
-  if ($routeParams.medium && !$routeParams.name) {
-    var media = {};
-    for (var a in $scope.all) {
-      if ($scope.all[a].medium.toLowerCase() == $routeParams.medium) {
-        media[a] = $scope.all[a];
-      }
+  creators.then(function(data) {
+    for (var d in data) {
+      $('body').append(
+        '<div class="creator-card"><a class="greyscale" href="#/content/' + data[d].medium + 
+        '/' + data[d].name + '"><h3>' + data[d].name + '</h3><a href="#/content/' + 
+        data[d].medium + '">' + data[d].medium + '</a><p id="' + data[d].name + '_clicks"></p></a></div>'
+      );
     }
-    $scope.all = media;
-  } else if ($routeParams.name) {
-    $scope.creator = $scope.all[$routeParams.name];
-  } else {
-    creators.then(function(data) {
-      for (var d in data) {
-        if (data[d].clicks) {
-          $('#' + d + '_clicks').append('<i class="fas fa-eye"></i> ' + data[d].clicks);
-        }
-      }
-    });
-  }
 }]);
