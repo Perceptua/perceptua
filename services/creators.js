3a0e97d3c6c33e8c7733192b7f7dcdbfdbd4f8c8
Avery Herkel <aherkel09@gmail.com>
2019-03-18 18:59:19 -0400

    add query handler

--- services/creators.js
+++ services/creators.js
@@ -2,7 +2,13 @@ app.factory('creators', ['$rootScope', '$q', function($rootScope, $q) {
   return {
     getCreators: function(status='public', query=null) {
       var deferred = $q.defer();
-      firebase.firestore().collection('creators').where('status', '==', 'public').get().then((docs) => {
+      var docs = firebase.firestore().collection('creators').where('status', '==', status);
+      
+      if (query) {
+        docs = docs.where(query.field, '==', query.value);
+      }
+      
+      docs.get().then((docs) => {
         var creators = {};
         docs.forEach((doc) => {
           creators[doc.id] = doc.data();
