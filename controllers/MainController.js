37fef349446902cd1a9f4e6a07cd403166f09df4
Avery Herkel <aherkel09@gmail.com>
2019-03-18 14:46:22 -0400

    simplify loop structure

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,40 +1,49 @@
 app.controller('MainController', ['$scope', '$routeParams', function($scope, $routeParams) { 
   $scope.title = 'perceptua';
   
-  $scope.featured = {
-    name: 'Aldous Huxley',
-    medium: 'Literature',
-  };
-
-  $scope.upcoming = {
+  $scope.all = {
+    'aldous_huxley': {
+      status: 'featured',
+      name: 'Aldous Huxley',
+      medium: 'Literature',
+  },
     'johannes_brahms': {
+      status: 'upcoming',
       name: 'Johannes Brahms',
       medium: 'Music',
-    }
-  };
-  
-  $scope.all = {
+    },
     'maxfield_parrish': {
+      status: 'published',
       name: 'Maxfield Parrish',
       medium: 'Art',
     },
     'stanley_kubrick': {
+      status: 'published',
       name: 'Stanley Kubrick',
       medium: 'Film',
     },
   };
   
-  $scope.media = ['Art', 'Film', 'Literature', 'Music'];
+  /* declare $scope variables */
+  $scope.media = [];
+  $scope.featured = {};
+  $scope.upcoming = {};
+  $scope.published = {};
   
-  if ($routeParams.medium && !$routeParams.name) {
-    var creators = {};
-    for (var a in $scope.all) {
-      if ($scope.all[a].medium.toLowerCase() == $routeParams.medium) {
-        creators[a] = $scope.all[a];
-      }
+  for (var a in $scope.all) {
+    var status = $scope.all[a].status;
+    if (status == 'featured') {
+      $scope.featured[a] = $scope.all[a];
+    } else if (status == 'upcoming') {
+      $scope.upcoming[a] = $scope.all[a];
+    /* if url matches /:medium, filter creators by medium param */
+    } else if ($routeParams.medium && !$routeParams.name && $scope.all[a].medium.toLowerCase() == $routeParams.medium) {
+      $scope.published[a] = $scope.all[a];
+    } else if ($routeParams.name && a == $routeParams.name) {
+      $scope.creator = $scope.all[a];
+    } else {
+      $scope.media.push($scope.all[a].medium);
+      $scope.published[a] = $scope.all[a];
     }
-    $scope.all = creators;
-  } else if ($routeParams.name) {
-    $scope.creator = $scope.all[$routeParams.name];
   }
 }]);
