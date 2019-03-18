247f04f81b2b831f58b18347b87fb309ca395ab9
Avery Herkel <aherkel09@gmail.com>
2019-03-18 19:13:26 -0400

    add q to scope

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,61 +1,64 @@
-app.controller('MainController', ['$scope', '$routeParams', 'creators', function($scope, $routeParams, creators) { 
-  $scope.title = 'perceptua';
-  /*
-  $scope.all = {
-    'aldous_huxley': {
-      status: 'featured',
-      name: 'Aldous Huxley',
-      medium: 'Literature',
-  },
-    'johannes_brahms': {
-      status: 'upcoming',
-      name: 'Johannes Brahms',
-      medium: 'Music',
+app.controller(
+  'MainController',
+  ['$scope', '$routeParams', '$rootScope', '$q', 'creators', function($scope, $routeParams, $rootScope, $q, creators) { 
+    $scope.title = 'perceptua';
+
+    /*
+    $scope.all = {
+      'aldous_huxley': {
+        status: 'featured',
+        name: 'Aldous Huxley',
+        medium: 'Literature',
     },
-    'maxfield_parrish': {
-      status: 'published',
-      name: 'Maxfield Parrish',
-      medium: 'Art',
-    },
-    'stanley_kubrick': {
-      status: 'published',
-      name: 'Stanley Kubrick',
-      medium: 'Film',
-    },
-  };
-  */
-  
-  creators.getCreators('public').then((data) => {
-    console.log(data);
-    $scope.all = data;
-  });
-  
-  /* declare $scope variables */
-  $scope.media = [];
-  $scope.featured = {};
-  $scope.upcoming = {};
-  $scope.published = {};
-  
-  for (var a in $scope.all) {
-    var status = $scope.all[a].status;
-    if (status == 'featured') {
-      $scope.featured[a] = $scope.all[a];
-    } else if (status == 'upcoming') {
-      $scope.upcoming[a] = $scope.all[a];
-    } else {
-      $scope.media.push($scope.all[a].medium);
-      $scope.published[a] = $scope.all[a];
-    }
-  }
-    
-  /* if url matches /:medium, filter published creators by medium param */
-  if ($routeParams.medium && !$routeParams.name) {
-    for (var p in $scope.published) {
-      if ($scope.published[p].medium.toLowerCase() != $routeParams.medium) {
-        delete $scope.published[p];
+      'johannes_brahms': {
+        status: 'upcoming',
+        name: 'Johannes Brahms',
+        medium: 'Music',
+      },
+      'maxfield_parrish': {
+        status: 'published',
+        name: 'Maxfield Parrish',
+        medium: 'Art',
+      },
+      'stanley_kubrick': {
+        status: 'published',
+        name: 'Stanley Kubrick',
+        medium: 'Film',
+      },
+    };
+
+    $scope.media = [];
+    $scope.featured = {};
+    $scope.upcoming = {};
+    $scope.published = {};
+
+    for (var a in $scope.all) {
+      var status = $scope.all[a].status;
+      if (status == 'featured') {
+        $scope.featured[a] = $scope.all[a];
+      } else if (status == 'upcoming') {
+        $scope.upcoming[a] = $scope.all[a];
+      } else {
+        $scope.media.push($scope.all[a].medium);
+        $scope.published[a] = $scope.all[a];
       }
     }
-  } else if ($routeParams.name) {
-    $scope.creator = $scope.all[$routeParams.name];
-  }
-}]);
+
+    if ($routeParams.medium && !$routeParams.name) {
+      for (var p in $scope.published) {
+        if ($scope.published[p].medium.toLowerCase() != $routeParams.medium) {
+          delete $scope.published[p];
+        }
+      }
+    } else if ($routeParams.name) {
+      $scope.creator = $scope.all[$routeParams.name];
+    }
+    */
+
+    $q((resolve, reject) => {
+      creators.getCreators('public').then((data) => {
+        console.log(data);
+        $scope.all = data;
+      });
+    });
+  }]);
