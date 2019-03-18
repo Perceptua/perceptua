1712fd5fe9bfea7e1cb4f47f2556eaa87d2e11cb
Avery Herkel <aherkel09@gmail.com>
2019-03-18 11:25:13 -0400

    get data from firebase

--- controllers/ContentController.js
+++ controllers/ContentController.js
@@ -1,18 +1,9 @@
 app.controller('ContentController', ['$scope', 'creators', '$routeParams', function($scope, creators, $routeParams) {
-  var content;
-  if ($routeParams.medium && !$routeParams.name) {
-    content = creators.getDocs('public', {field: 'medium', value: $routeParams.medium});
-  } else {
-    content = creators.getDocs('public');
-  }
-  
-  content.then(function(data) {
-    for (var d in data) {
-      $('body').append(
-        '<div class="creator-card"><a class="greyscale" href="#/content/' + data[d].medium + 
-        '/' + data[d].name + '"><h3>' + data[d].name + '</h3><a href="#/content/' + 
-        data[d].medium + '">' + data[d].medium + '</a><p id="' + data[d].name + '_clicks"></p></a></div>'
-      );
-    }
+  firebase.firestore().collection('creators').where('status', '==', 'public').get().then(function(docs) {
+    $scope.all = {};
+    docs.forEach(function(doc) {
+      $scope.all[doc.id] = doc.data();
+    });
+    return $scope.all;
   });
 }]);
