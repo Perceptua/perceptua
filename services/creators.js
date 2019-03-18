f4594ee523a66f06e087854e89821f0dd49d111e
Avery Herkel <aherkel09@gmail.com>
2019-03-18 10:29:37 -0400

    update database queries

--- services/creators.js
+++ services/creators.js
@@ -1,5 +1,16 @@
-app.factory('creators', [function() {
-  return firebase.firestore().collection('creators').get().then(function(docs) {
+app.factory('creators', [function(subset=null, query=null) {
+  var docs;
+  if (subset && !query) {
+    docs = firebase.firestore().collection('creators').where('status', '==', subset);
+  } else if (subset && query) {
+    docs = firebase.firestore().collection('creators').where('staus', '==', subset).where(query.field, '==', query.value);
+  } else if (!subset && query) {
+    docs = firebase.firestore().collection('creators').where(query.field, '==', query.value);
+  } else {
+    docs = firebase.firestore().collection('creators');
+  }
+  
+  docs.get().then(function(docs) {
     var creators = {};
     docs.forEach(function(doc) {
       creators[doc.id] = doc.data();
