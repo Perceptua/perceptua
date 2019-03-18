99652cc358947a2b81e03647ce09e74c5fccdbf1
Avery Herkel <aherkel09@gmail.com>
2019-03-18 09:34:11 -0400

    Update ContentController.js

--- controllers/ContentController.js
+++ controllers/ContentController.js
@@ -1,8 +1,34 @@
 app.controller('ContentController', ['$scope', 'creators', '$routeParams', function($scope, creators, $routeParams) {
-  creators.then(function(data) {
-    if ($routeParams.name) {
-      console.log(data[$routeParams.name]);
-      $scope.creator = data[$routeParams.name];
+  $scope.all = {
+    'maxfield_parrish': {
+      name: 'Maxfield Parrish',
+      medium: 'Art',
+    },
+    'stanley_kubrick': {
+      name: 'Stanley Kubrick',
+      medium: 'Film',
+    },
+  };
+  
+  $scope.featured = {
+    name: 'Aldous Huxley',
+    medium: 'Literature',
+  };  
+    
+  if ($routeParams.medium && !$routeParams.name) {
+    $scope.creators = {};
+    for (var a in $scope.all) {
+      if ($scope.all[a].medium == $routeParams.medium) {
+        $scope.creators[a] = $scope.all[a];
+      }
     }
-  });
+  } else if ($routeParams.name) {
+    $scope.creator = $scope.all[$routeParams.name];
+  } else {
+    creators.then(function(data) {
+      for (var d in data) {
+        $('#' + d + '_clicks').text(data[d].clicks);
+      }
+    });
+  }
 }]);
