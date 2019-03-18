2ea0a63745dd0793a4ba1fcdb9832184aa557636
Avery Herkel <aherkel09@gmail.com>
2019-03-18 11:35:40 -0400

    get public creator data

--- services/creators.js
+++ services/creators.js
@@ -1,3 +1,9 @@
 app.factory('creators', [function() {
-  return firebase.firestore().collection('creators');
+  return firebase.firestore().collection('creators').where('status', '==', 'public').get().then(function(docs) {
+    var creators = {};
+    docs.forEach(function(doc) {
+      creators[doc.id] = doc.data();
+    });
+    return creators;
+  });
 }]);
