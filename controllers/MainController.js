c0ac3a22c0564bc7c4e42bc41a971a0e34e3a5a9
Avery Herkel <aherkel09@gmail.com>
2019-03-17 21:44:20 -0400

    Update MainController.js

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,6 +1,13 @@
 app.controller('MainController', ['$scope', 'creators', function($scope, creators) { 
   $scope.title = 'perceptua';
   
+  $scope.all = [
+      {
+        name: 'Maxfield Parrish',
+        medium: 'Art',
+      },
+    ];
+  
   creators.then(function(data) {
     $scope.all = [
       {
