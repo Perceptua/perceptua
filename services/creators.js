45d82aa941235667f63e90cf0eac174c1d598108
Avery Herkel <aherkel09@gmail.com>
2019-03-18 11:13:15 -0400

    wrap data retrieval

--- services/creators.js
+++ services/creators.js
@@ -1,20 +1,22 @@
-app.factory('creators', [function(subset=null, query=null) {
-  var docs;
-  if (subset && !query) {
-    docs = firebase.firestore().collection('creators').where('status', '==', subset);
-  } else if (subset && query) {
-    docs = firebase.firestore().collection('creators').where('staus', '==', subset).where(query.field, '==', query.value);
-  } else if (!subset && query) {
-    docs = firebase.firestore().collection('creators').where(query.field, '==', query.value);
-  } else {
-    docs = firebase.firestore().collection('creators');
-  }
-  
-  return docs.get().then(function(docs) {
-    var creators = {};
-    docs.forEach(function(doc) {
-      creators[doc.id] = doc.data();
+app.factory('creators', [function() {
+  function getDocs(subset='public', query=null) {
+    var docs;
+    if (subset && !query) {
+      docs = firebase.firestore().collection('creators').where('status', '==', subset);
+    } else if (subset && query) {
+      docs = firebase.firestore().collection('creators').where('staus', '==', subset).where(query.field, '==', query.value);
+    } else if (!subset && query) {
+      docs = firebase.firestore().collection('creators').where(query.field, '==', query.value);
+    } else {
+      docs = firebase.firestore().collection('creators');
+    }
+
+    return docs.get().then(function(docs) {
+      var creators = {};
+      docs.forEach(function(doc) {
+        creators[doc.id] = doc.data();
+      });
+      return creators;
     });
-    return creators;
-  });
+  }
 }]);
