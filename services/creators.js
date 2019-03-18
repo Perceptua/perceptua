27110213185846d327a26a369c3ad38735176fe0
Avery Herkel <aherkel09@gmail.com>
2019-03-18 08:24:57 -0400

    Update creators.js

--- services/creators.js
+++ services/creators.js
@@ -1,11 +1,9 @@
-app.factory('creators', ['$http', function($http) {
-  var deferred = $q.defer();
-  firebase.firestore().collection('creators').get().then(function(docs) {
+app.factory('creators', [function() {
+  return firebase.firestore().collection('creators').get().then(function(docs) {
     var creators = {};
     docs.forEach(function(doc) {
       creators[doc.id] = doc.data();
     });
-    deferred.resolve(creators);
-    return deferred.promise;
+    return creators;
   });
 }]);
