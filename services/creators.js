3680857a071f35b508d4cb40c7492bd1d38cff8e
Avery Herkel <aherkel09@gmail.com>
2019-03-17 21:44:55 -0400

    Update creators.js

--- services/creators.js
+++ services/creators.js
@@ -2,7 +2,6 @@ app.factory('creators', [function() {
   return firebase.firestore().collection('creators').get().then(function(docs) {
     var creators = {};
     docs.forEach(function(doc) {
-      console.log(doc.data());
       creators[doc.id] = doc.data();
     });
     return creators;
