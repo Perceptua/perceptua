d81ec58f9c07a40b18c725aa8a51eadcdc51805c
Avery Herkel <aherkel09@gmail.com>
2019-03-17 23:05:47 -0400

    defer

--- services/creators.js
+++ services/creators.js
@@ -1,5 +1,11 @@
-app.factory('creators', [function() {
-  return firebase.firestore().collection('creators').get().then(function(docs) {
-    return docs;
+app.factory('creators', [function($q) {
+  var deferred = $q.defer();
+  firebase.firestore().collection('creators').get().then(function(docs) {
+    var creators = {};
+    docs.forEach(function(doc) {
+      creators[doc.id] = doc.data();
+    });
+    deferred.resolve(creators);
+    return deferred.promise;
   });
 }]);
