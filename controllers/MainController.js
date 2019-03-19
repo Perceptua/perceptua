a594a5c851e35386400c42784ee0184728bc648b
Avery Herkel <aherkel09@gmail.com>
2019-03-19 16:41:28 -0400

    set filters from route params

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,5 +1,11 @@
 app.controller('MainController', ['$scope', '$routeParams', 'creators', function($scope, $routeParams, creators) {
-  var filter = $routeParams.medium || null;
+  var filter = null;
+  
+  if ($routeParams.medium && !$routeParams.name) {
+    filter = {medium: $routeParams.medium};
+  } else if ($routeParams.name) {
+    filter = {name: $routeParams.name};
+  }
   
   creators.getCreators('published', filter).then((data) => {
     $scope.published = data;
