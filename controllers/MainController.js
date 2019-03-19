cce8115c8c8e78012145bb8b327c74c35b934b75
Avery Herkel <aherkel09@gmail.com>
2019-03-19 15:53:03 -0400

    remove logs, call service

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -6,19 +6,14 @@ app.controller('MainController', ['$scope', '$routeParams', 'creators', function
   var statusArray = ['featured', 'upcoming', 'published'];
   
   for (var s in statusArray) {
-    console.log('index: ' + s);
     var status = statusArray[s];
-    console.log('list item: ' + status);
-    /*
     creators.getCreators(status).then((docs) => {
       $scope[status] = {};
       docs.forEach((doc) => {
         $scope[status][doc.id] = doc.data();
       });
-      console.log(status + ': ');
       console.log($scope[status]);
     });
-    */
   }
   
 }]);
