ce15a34e5b0c8cdf250de8819655049e5af96dab
Avery Herkel <aherkel09@gmail.com>
2019-03-19 12:07:49 -0400

    return $q promise

--- services/creators.js
+++ services/creators.js
@@ -1,18 +1,16 @@
 app.factory('creators', ['$rootScope', '$q', function($rootScope, $q) {
   return {
     getCreators: function(status='public', query=null) {
-      console.log('generating query...');
-      var docs = firebase.firestore().collection('creators').where('status', '==', status);
-      
-      if (query) {
-        docs = docs.where(query.field, '==', query.value);
-      }
-      
-      console.log('executing query...');
-      
-      return $q((resolve) => {
-        console.log('returning promise...');
-        resolve(docs);
+      return $q((resolve, reject) => {
+        console.log('generating query...');
+        var docs = firebase.firestore().collection('creators').where('status', '==', status);
+
+        if (query) {
+          docs = docs.where(query.field, '==', query.value);
+        }
+
+        console.log('executing query...');
+        resolve(docs.get());
       });
     }
   };
