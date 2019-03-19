4036900162b36d9b1c1f56694fba96c7708c981e
Avery Herkel <aherkel09@gmail.com>
2019-03-19 10:03:24 -0400

    return promise

--- services/creators.js
+++ services/creators.js
@@ -1,7 +1,9 @@
 app.factory('creators', ['$rootScope', '$q', function($rootScope, $q) {
   return {
     getCreators: function(status='public', query=null) {
-      console.log('generating promise...');
+      console.log('generating query...');
+      var creators = {};
+      var deferred = $q.defer();
       var docs = firebase.firestore().collection('creators').where('status', '==', status);
       
       if (query) {
@@ -9,7 +11,21 @@ app.factory('creators', ['$rootScope', '$q', function($rootScope, $q) {
       }
       
       console.log('executing query...');
-      return docs.get();
+      docs.get().then((docs) => {
+        console.log('retrived docs...');
+        docs.forEach((doc) => {
+          console.log('looping through docs...');
+          creators[doc.id] = doc.data();
+        });
+        console.log('resolving promise...');
+        deferred.resolve(creators);
+      }).catch((error) => {
+        console.log('rejecting promise...');
+        deferred.reject(error);
+      });
+      
+      console.log('returning promise...');
+      return deferred.promise;
     }
   };
 }]);
