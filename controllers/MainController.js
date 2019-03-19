bff059f0b8c292bb34082ea1de4c35d5cfbf186a
Avery Herkel <aherkel09@gmail.com>
2019-03-19 15:42:03 -0400

    log data

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -6,7 +6,9 @@ app.controller('MainController', ['$scope', '$routeParams', 'creators', function
   var statusArray = ['featured', 'upcoming', 'published'];
   
   for (var s in statusArray) {
+    console.log('index: ' + s);
     var status = statusArray[s];
+    console.log('list item: ' + status);
     creators.getCreators(status).then((docs) => {
       $scope[status] = {};
       docs.forEach((doc) => {
