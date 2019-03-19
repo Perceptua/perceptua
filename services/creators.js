635d4b7265543ea9c405fe84cfd47b7dafff26bc
Avery Herkel <aherkel09@gmail.com>
2019-03-19 16:42:36 -0400

    update arg name

--- services/creators.js
+++ services/creators.js
@@ -1,11 +1,13 @@
 app.factory('creators', ['$rootScope', '$q', function($rootScope, $q) {
   return {
-    getCreators: function(status, query=null) {
+    getCreators: function(status, filter) {
       return $q((resolve, reject) => {
         var docs = firebase.firestore().collection('creators').where('status', '==', status);
-        if (query) {
-          docs = docs.where(query.field, '==', query.value);
+        
+        if (filter) {
+          docs = docs.where(filter.field, '==', filter.value);
         }
+        
         docs.get().then((docs) => {
           var creators = {};
           docs.forEach((doc) => {
