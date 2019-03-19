377c612e88454ac923d0ba2fd5269e9ab3cd2bf3
Avery Herkel <aherkel09@gmail.com>
2019-03-19 09:25:11 -0400

    log data at steps

--- services/creators.js
+++ services/creators.js
@@ -1,6 +1,7 @@
 app.factory('creators', ['$rootScope', '$q', function($rootScope, $q) {
   return {
     getCreators: function(status='public', query=null) {
+      console.log('getting creators...');
       var deferred = $q.defer();
       var docs = firebase.firestore().collection('creators').where('status', '==', status);
       
@@ -9,11 +10,14 @@ app.factory('creators', ['$rootScope', '$q', function($rootScope, $q) {
       }
       
       return docs.get().then((docs) => {
+        console.log('retrieved docs...');
         var creators = {};
         docs.forEach((doc) => {
+          console.log('looping through docs...');
           creators[doc.id] = doc.data();
         });
         deferred.resolve(creators);
+        console.log('resolved promise...');
         return deferred.promise;
       }).catch((error) => {
         deferred.reject(error);
