f67812661672538939ba4b4963389ddb454bed7e
Avery Herkel <aherkel09@gmail.com>
2019-03-19 14:42:21 -0400

    use routeParams in function args

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,63 +1,13 @@
 app.controller('MainController', ['$scope', '$routeParams', 'creators', function($scope, $routeParams, creators) { 
   $scope.title = 'perceptua';
-
-  /*
-  $scope.all = {
-    'aldous_huxley': {
-      status: 'featured',
-      name: 'Aldous Huxley',
-      medium: 'Literature',
-  },
-    'johannes_brahms': {
-      status: 'upcoming',
-      name: 'Johannes Brahms',
-      medium: 'Music',
-    },
-    'maxfield_parrish': {
-      status: 'published',
-      name: 'Maxfield Parrish',
-      medium: 'Art',
-    },
-    'stanley_kubrick': {
-      status: 'published',
-      name: 'Stanley Kubrick',
-      medium: 'Film',
-    },
-  };
-
-  $scope.media = [];
-  $scope.featured = {};
-  $scope.upcoming = {};
-  $scope.published = {};
-
-  for (var a in $scope.all) {
-    var status = $scope.all[a].status;
-    if (status == 'featured') {
-      $scope.featured[a] = $scope.all[a];
-    } else if (status == 'upcoming') {
-      $scope.upcoming[a] = $scope.all[a];
-    } else {
-      $scope.media.push($scope.all[a].medium);
-      $scope.published[a] = $scope.all[a];
-    }
-  }
-
-  if ($routeParams.medium && !$routeParams.name) {
-    for (var p in $scope.published) {
-      if ($scope.published[p].medium.toLowerCase() != $routeParams.medium) {
-        delete $scope.published[p];
-      }
-    }
-  } else if ($routeParams.name) {
-    $scope.creator = $scope.all[$routeParams.name];
-  }
-  */
   
-  creators.getCreators('public').then((docs) => {
-    $scope.published = {};
-    docs.forEach((doc) => {
-      $scope.published[doc.id] = doc.data();
+  if ($routeParams.status) {
+    creators.getCreators($routeParams.status).then((docs) => {
+      $scope[$routeParams.status] = {};
+      docs.forEach((doc) => {
+        $scope[$routeParams.status[doc.id]] = doc.data();
+      });
     });
-  });
+  } 
   
 }]);
