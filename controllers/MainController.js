f39ad7669834b704abb096467943b356b8b98d22
Avery Herkel <aherkel09@gmail.com>
2019-03-19 16:26:54 -0400

    get data from service

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,23 +1,8 @@
 app.controller('MainController', ['$scope', '$routeParams', 'creators', function($scope, $routeParams, creators) { 
   $scope.title = 'perceptua';
   
-  console.log($routeParams);
-  
-  var statusArray = ['featured', 'upcoming', 'published'];
-  
-  for (var s in statusArray) {
-    var status = statusArray[s];
-    creators.getCreators(status).then((docs) => {
-      $scope[status] = {};
-      docs.forEach((doc) => {
-        $scope[status][doc.id] = doc.data();
-      });
-      console.log($scope);
-    });
-  }
-  
-  console.log($scope.featured);
-  console.log($scope.upcoming);
-  console.log($scope.published);
-  
+  creators.getCreators('published').then((data) => {
+    $scope.published = data;
+    console.log($scope.published);
+  });
 }]);
