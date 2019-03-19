bc6723014430f0194526e989b6198bfb7dad3c35
Avery Herkel <aherkel09@gmail.com>
2019-03-19 19:27:33 -0400

    Create name.js

---
+++ services/name.js
@@ -0,0 +1,18 @@
+app.factory('name', ['$rootScope', '$q', function($rootScope, $q) {
+  return {
+    convertToName: function(string) {
+      var name = '';
+      var allNames = string.split('_');
+      for (var a in allNames) {
+        if (allNames[a].length == 1) {
+          allNames[a] += '.'
+        }
+        name += allNames[a];
+        if (a < allNames.length - 1) {
+          name += ' ';
+        }
+      }
+      return name;
+    },
+  };
+});
