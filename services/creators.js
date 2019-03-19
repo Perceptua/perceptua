14d14060e3948795854b512bf0351868dcd9be24
Avery Herkel <aherkel09@gmail.com>
2019-03-19 12:16:34 -0400

    return creators

--- services/creators.js
+++ services/creators.js
@@ -1,17 +1,24 @@
 app.factory('creators', ['$rootScope', '$q', function($rootScope, $q) {
   return {
     getCreators: function(status='public', query=null) {
-      return $q((resolve, reject) => {
-        console.log('generating query...');
-        var docs = firebase.firestore().collection('creators').where('status', '==', status);
+      console.log('generating query...');
+      var docs = firebase.firestore().collection('creators').where('status', '==', status);
 
-        if (query) {
-          docs = docs.where(query.field, '==', query.value);
-        }
+      if (query) {
+        docs = docs.where(query.field, '==', query.value);
+      }
 
-        console.log('executing query...');
-        resolve(docs.get());
+      console.log('executing query...');
+      var creators = {};
+      docs.get().then((docs) => {
+        console.log('looping over docs...');
+        docs.forEach((doc) => {
+          creators[doc.id] = doc.data();
+        });
       });
+      
+      console.log(creators);
+      return creators;
     }
   };
 }]);
