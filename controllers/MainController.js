28c88255c16f659a548b0d7f943fea4f0bbf32dd
Avery Herkel <aherkel09@gmail.com>
2019-03-19 19:37:33 -0400

    get name from creators service

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,11 +1,10 @@
-app.controller('MainController', ['$scope', '$routeParams', 'creators', 'name', function($scope, $routeParams, creators, name) {
+app.controller('MainController', ['$scope', '$routeParams', 'creators', function($scope, $routeParams, creators) {
   var filter = null;
   
   if ($routeParams.medium && !$routeParams.name) {
     filter = {field: 'medium', value: $routeParams.medium};
   } else if ($routeParams.name) {
-    console.log(name.convertToName($routeParams.name));
-    filter = {field: 'name', value: name.convertToName($routeParams.name)};
+    filter = {field: 'name', value: creators.getFullName($routeParams.name)};
   }
   
   creators.getCreators($routeParams.status, filter).then((data) => {
