467e26c872cac8a8844cc3306595358782d47732
Avery Herkel <aherkel09@gmail.com>
2019-03-19 14:31:51 -0400

    alter urls

--- apps/perceptua.js
+++ apps/perceptua.js
@@ -2,28 +2,22 @@ var app = angular.module('perceptua', ['ngRoute']);
 
 app.config(function($routeProvider) {
   $routeProvider
-  .when("/featured", {
-    templateUrl: "views/featured.html"
-  })
-  .when("/upcoming", {
-    templateUrl: "views/upcoming.html"
-  })
-  .when("/all", {
-    templateUrl: "views/all.html"
-  })
   .when("/suggest", {
     templateUrl: "views/suggest.html"
   })
   .when("/about", {
     templateUrl: "views/about.html"
   })
-  .when("/content/:medium", {
+  .when("/content/:status", {
     templateUrl: "views/all.html"
   })
-  .when("/content/:medium/:name", {
+  .when("/content/:status/:medium", {
+    templateUrl: "views/all.html"
+  })
+  .when("/content/:status/:medium/:name", {
     templateUrl: "views/content.html"
   })
   .otherwise({
-    redirectTo: "/featured"
+    redirectTo: "/about"
   });
 });
