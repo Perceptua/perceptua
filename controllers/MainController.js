18743b32b45600fa841a8fc9163f32f1bc4da0ef
Avery Herkel <aherkel09@gmail.com>
2019-03-19 16:33:10 -0400

    filter data

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,8 +1,7 @@
-app.controller('MainController', ['$scope', '$routeParams', 'creators', function($scope, $routeParams, creators) { 
-  $scope.title = 'perceptua';
+app.controller('MainController', ['$scope', '$routeParams', 'creators', function($scope, $routeParams, creators) {
+  var filter = $routeParams.medium || null;
   
-  creators.getCreators('published').then((data) => {
+  creators.getCreators('published', filter).then((data) => {
     $scope.published = data;
-    console.log($scope.published);
   });
 }]);
