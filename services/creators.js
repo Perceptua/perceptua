f68ef3aac3398ba4074163d2934feba0f18c26ec
Avery Herkel <aherkel09@gmail.com>
2019-03-19 15:20:37 -0400

    remove logs

--- services/creators.js
+++ services/creators.js
@@ -1,15 +1,11 @@
 app.factory('creators', ['$rootScope', '$q', function($rootScope, $q) {
   return {
-    getCreators: function(status='public', query=null) {
+    getCreators: function(status, query=null) {
       return $q((resolve, reject) => {
-        console.log('generating query...');
         var docs = firebase.firestore().collection('creators').where('status', '==', status);
-
         if (query) {
           docs = docs.where(query.field, '==', query.value);
         }
-
-        console.log('executing query...');
         resolve(docs.get());
       });
     },
