4ff5effc080d6a6d3460f865825e031f798025ba
Avery Herkel <aherkel09@gmail.com>
2019-03-20 10:12:33 -0400

    add support for data ordering, change param name

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -2,13 +2,24 @@ app.controller('MainController', ['$scope', '$routeParams', 'creators', function
   $scope.status = $routeParams.status;
   
   var filter = null;
-  if ($routeParams.medium && !$routeParams.name) {
-    filter = {field: 'medium', value: $routeParams.medium};
+  var orderBy = null;
+  
+  if ($routeParams.sort && !$routeParams.name) {
+    filter = {field: 'medium', value: $routeParams.sort};
+    
+    if ($routeParams.sort == 'newer') {
+      orderBy = {field: 'date', order: 'desc'};
+    } else if ($routeParams.sort == 'older') {
+      orderBy = {field: 'date', order: 'asc'};
+    } else if ($routeParams.sort == 'clicks') {
+      orderBy = {field: 'clicks', order: 'desc'};
+    }
+    
   } else if ($routeParams.name) {
     filter = {field: 'name', value: creators.getFullName($routeParams.name)};
   }
   
-  creators.getCreators($scope.status, filter).then((data) => {
+  creators.getCreators($scope.status, filter, orderBy).then((data) => {
     $scope.creators = data.creators;
     $scope.media = data.media
   });
