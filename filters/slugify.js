8e85c7389d80a8fde6bfbfd4deaea341e230bce4
Avery Herkel <aherkel09@gmail.com>
2019-03-20 13:10:05 -0400

    check if input is string

--- filters/slugify.js
+++ filters/slugify.js
@@ -1,15 +1,18 @@
 app.filter('slugify', function() {
   return function(string) {
-    var words = string.replace('.', '').split(' ');
-    var slug = '';
+    if (typeof string === 'string' || string instanceof String) {
+      var words = string.replace('.', '').split(' ');
+      var slug = '';
 
-    for (var w in words) {
-      slug += words[w].toLowerCase();
-      if (w < words.length - 1) {
-        slug += '_';
+      for (var w in words) {
+        slug += words[w].toLowerCase();
+        if (w < words.length - 1) {
+          slug += '_';
+        }
       }
-    }
 
-    return slug;
+      return slug;
+    }
+    return '';
   };
 });
