d194a64aa76b4de4b5e225b94f91cbceab0c9cc2
Avery Herkel <aherkel09@gmail.com>
2019-03-20 11:39:59 -0400

    fadeToggle

--- directives/access.js
+++ directives/access.js
@@ -56,7 +56,7 @@ app.directive('access', function() {
       }
       
       scope.showAccessHelp = function() {
-        $('#access-help').toggleFade('slow');
+        $('#access-help').fadeToggle('slow');
       }
       
     } 
