d4f95445eb48d46eba71bef23d419872d006e493
Avery Herkel <aherkel09@gmail.com>
2019-03-20 09:34:18 -0400

    set status var

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,13 +1,14 @@
 app.controller('MainController', ['$scope', '$routeParams', 'creators', function($scope, $routeParams, creators) {
-  var filter = null;
+  $scope.status = $routeParams.status;
   
+  var filter = null;
   if ($routeParams.medium && !$routeParams.name) {
     filter = {field: 'medium', value: $routeParams.medium};
   } else if ($routeParams.name) {
     filter = {field: 'name', value: creators.getFullName($routeParams.name)};
   }
   
-  creators.getCreators($routeParams.status, filter).then((data) => {
+  creators.getCreators($scope.status, filter).then((data) => {
     $scope.creators = data.creators;
     $scope.media = data.media
   });
