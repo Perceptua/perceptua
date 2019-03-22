d08712f774aefe43a1708f1247041bc45c923047
Avery Herkel <aherkel09@gmail.com>
2019-03-22 11:32:54 -0400

    add comments

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,13 +1,15 @@
 app.controller('MainController', ['$scope', '$routeParams', 'creators', function($scope, $routeParams, creators) {
+  // can be 'featured', 'upcoming', or 'published'
   $scope.status = $routeParams.status;
   
+  // initialize filter & ordering for databse query
   var filter = null;
   var orderBy = {field: 'added', order: 'desc'};
+  
+  // update query from $routeParams of requested resource
   if ($routeParams.sort && !$routeParams.name) {
     if ($routeParams.sort == 'older') {
       orderBy = {field: 'added', order: 'asc'};
-    } else if ($routeParams.sort == 'clicks') {
-      orderBy = {field: 'clicks', order: 'desc'};
     } else {
       filter = {field: 'medium', value: $routeParams.sort};
     }
@@ -15,6 +17,7 @@ app.controller('MainController', ['$scope', '$routeParams', 'creators', function
     filter = {field: 'name', value: creators.getFullName($routeParams.name)};
   }
   
+  // send query to service, include results in scope
   creators.getCreators($scope.status, filter, orderBy).then((data) => {
     $scope.creators = data.creators;
     $scope.media = data.media
