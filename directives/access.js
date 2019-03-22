f956b90b6c01cbeacb940b32c7688df18869434d
Avery Herkel <aherkel09@gmail.com>
2019-03-22 11:36:00 -0400

    add comments

--- directives/access.js
+++ directives/access.js
@@ -4,7 +4,8 @@ app.directive('access', function() {
     scope: {}, 
     templateUrl: 'directives/access.html',
     link: function(scope, element, attrs) {
-            
+      
+      // animation to display featured content
       scope.grantAccess = function(url) {
         $('#access-loader').fadeOut('slow');
         $('access').first().fadeOut('slow');
@@ -22,6 +23,7 @@ app.directive('access', function() {
         });
       }
       
+      // notify user of error/incorrect code
       scope.failAccess = function(error=false) {
         $('#access-loader').fadeOut('slow');
         if (!error) {
@@ -33,7 +35,8 @@ app.directive('access', function() {
         $('#access-input').css('border', '0.25em solid #990033');
         $('#error').append('<p style="font-size:0.75em;">' + text + 'Please try again.' + '</p>');
       }
-              
+      
+      // send request with access code to firebase function endpoint
       scope.checkAccess = function() {
         $('#error').empty();
         $('#access-loader').fadeIn('slow');
@@ -58,6 +61,7 @@ app.directive('access', function() {
         });
       }
       
+      // show info text
       scope.showAccessHelp = function() {
         $('#access-help').fadeToggle('slow');
       }
