4f25229c7def88556610deb97579cee3686f0156
Avery Herkel <aherkel09@gmail.com>
2019-06-14 13:39:30 -0400

    replace all non-alphanumeric

--- filters/slugify.js
+++ filters/slugify.js
@@ -1,6 +1,19 @@
 app.filter('slugify', function() {
-  // replace all non-alphanumeric characters with a hyphen
+  // convert title to slug
   return function(string) {
-    return string.replace(/[\W_]+/g, '-');
-  }
+    if (typeof string === 'string' || string instanceof String) {
+      var words = string.split(' ');
+      var slug = '';
+
+      for (var w in words) {
+        slug += words[w].replace(/[\W_]+/g, '').toLowerCase();
+        if (w < words.length - 1) {
+          slug += '-';
+        }
+      }
+
+      return slug;
+    }
+    return '';
+  };
 });
