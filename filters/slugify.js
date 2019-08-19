87a63f2cdc25593904b19e90db63639e5fa7a81b
Avery Herkel <perceptua.blue@gmail.com>
2019-08-19 09:48:25 -0400

    reorder replacements

--- filters/slugify.js
+++ filters/slugify.js
@@ -6,7 +6,7 @@ app.filter('slugify', function() {
       var slug = '';
 
       for (var w in words) {
-        slug += words[w].replace(/[\W_]+/g, '').replace('&', 'and').toLowerCase();
+        slug += words[w].replace('&', 'and').replace(/[\W_]+/g, '').toLowerCase();
         if (w < words.length - 1) {
           slug += '-';
         }
