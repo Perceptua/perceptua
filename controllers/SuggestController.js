fd091ebbd3d9e10038ee9b70ab3dbe2ce138cf98
Avery Herkel <perceptua.blue@gmail.com>
2019-09-09 08:44:01 -0400

    Delete SuggestController.js

--- controllers/SuggestController.js
+++
@@ -1,9 +0,0 @@
-app.controller('SuggestController', ['$scope', '$routeParams', 'suggest', function($scope, $routeParams, suggest) {
-
-  suggest.getSuggestions().then((data) => {
-    $scope.content = data.content;
-    $scope.creators = data.creators;
-    $scope.media = data.media;
-  });
-  
-}]);
