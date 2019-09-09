c3f1c3be5437aef98d1431f6af6172a3f79ad9d5
Avery Herkel <perceptua.blue@gmail.com>
2019-09-09 07:43:53 -0400

    update controller

--- apps/perceptua.js
+++ apps/perceptua.js
@@ -3,6 +3,7 @@ var app = angular.module('perceptua', ['ngRoute']);
 app.config(function($routeProvider) {
   $routeProvider
   .when("/suggest", {
+    controller: "SuggestController",
     templateUrl: "views/suggest.html",
   })
   .when("/about", {
