8b7494e0043c9d12d38a0c5f604ecd0581e5c98e
Avery Herkel <perceptua.blue@gmail.com>
2019-09-11 10:04:54 -0400

    move form nav function to static/ui.js

--- directives/suggest.js
+++ directives/suggest.js
@@ -106,34 +106,5 @@ function fillField(field, value) {
 }
 
 $(document).ready(function() {
-  $('.suggest-field').keydown(function(ev) {
-    var items = $('.item');
-    var selected = items.filter('.active');
-    var current;
-    
-    if (!items.length > 0) { return false; }
-    items.removeClass('active');
-    
-    if (ev.keyCode == 40) { // down arrow
-      if (!selected.length || selected.is(':last-child') ) {
-        current = items.first();
-      }
-      else {
-        current = selected.next();
-      }
-    } else if (ev.keyCode == 38) { // up arrow
-      if (!selected.length || selected.is(':first-child') ) {
-        current = items.last();
-      }
-      else {
-        current = selected.prev();
-      }
-    } else if (ev.keyCode == 13) { // enter
-      $(selected).first().click();
-    } else {
-      return false;
-    }
-    
-    $(current).addClass('.active');
-  });
+  addFormNav('.suggest-field');
 });
