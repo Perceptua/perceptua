714e68431b28a4d95be2eae441b791faf9b26ec8
Avery Herkel <perceptua.blue@gmail.com>
2019-11-01 07:26:20 -0400

    update url params

--- apps/perceptua.js
+++ apps/perceptua.js
@@ -8,23 +8,28 @@ app.config(function($routeProvider) {
   .when("/about", {
     templateUrl: "views/about.html",
   })
-  .when("/:status", { // choice of featured, upcoming, or published
+  .when("/:status", {
     controller: "MainController",
     templateUrl: function(params) {
       return "views/" + params.status + ".html";
     },
   })
-  .when("/:status/:sort", {
+  .when("/:status/:filter", {
     controller: "MainController",
     templateUrl: function(params) {
       return "views/" + params.status + ".html";
     },
   })
-  .when("/:status/:sort/:title", { // show selected upcoming content (published handled by Jekyll in _posts)
+  .when("/:status/:filter/:select", {
     controller: "MainController",
-    templateUrl: "views/content.html",
+    templateUrl: function(params) {
+      if (params.select == 'older') {
+        return "views/" + params.status + ".html";
+      }
+      return "views/content.html"; // show selected content
+    },
   })
   .otherwise({
-    redirectTo: "/published",
+    redirectTo: "/published/all/",
   });
 });
