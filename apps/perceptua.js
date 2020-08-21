4c471b58cf117c2726f300dcd16a7b6e1488f70c
Perceptua <perceptua.blue@gmail.com>
2020-08-20 20:10:15 -0400

    remove suggestions

--- apps/perceptua.js
+++ apps/perceptua.js
@@ -2,9 +2,6 @@ var app = angular.module('perceptua', ['ngRoute']);
 
 app.config(function($routeProvider) {
   $routeProvider
-  .when("/suggest", {
-    templateUrl: "views/suggest.html",
-  })
   .when("/about", {
     templateUrl: "views/about.html",
   })
