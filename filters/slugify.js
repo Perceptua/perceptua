68dd7a95f4fa66e8ee797837104f93f73b3ecb4d
Perceptua <perceptua.blue@gmail.com>
2021-07-14 17:52:33 -0400

    remove regex

--- filters/slugify.js
+++ filters/slugify.js
@@ -6,7 +6,7 @@ app.filter('slugify', function() {
       var slug = '';
 
       for (var w in words) {
-        slug += words[w].replace('&', 'and').replace(/[_]+/g, '').toLowerCase();
+        slug += words[w].replace('&', 'and').toLowerCase();
         if (w < words.length - 1) {
           slug += '-';
         }
